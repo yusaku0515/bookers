ec73107622bfed0a49fba2612d6179a25a8317af
yusaku0515 <y.iwata0515@gmail.com>
2020-04-13 01:05:02 +0900

    4-8[finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      @books = Book.all
+      render "index"
+    end
   end
 
   def destroy
@@ -26,9 +30,13 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      render "edit"
+    end
   end
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+    validates :body, presence: true
+    validates :title, presence: true
 end
